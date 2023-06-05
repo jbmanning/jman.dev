7f8a574d1c9742fdc9655ab0437b13768563f9b2
Josh Manning <josh@jman.me>
2023-06-04 22:02:22 -0700

    init monorepo

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,9 @@
+export default [
+	'eslint:recommended',
+	'plugin:@typescript-eslint/recommended',
+	'plugin:import/recommended',
+	'next',
+	'plugin:react/recommended',
+	'plugin:react-hooks/recommended',
+	'prettier',
+];
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "always",
+  trailingComma: "es5",
+  singleQuote: true,
+  bracketSpacing: false,
+  printWidth: 90,
+  useTabs: true,
+  tabWidth: 2,
+  semi: true,
+};
